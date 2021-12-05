91bc2c5248f31ea055cba8b59570b65cf73447c6
ikmalismail17 <ikmal5a2013@gmail.com>
2021-12-05 23:19:24 +0800

    edit sikit2

--- lib/time_solat.dart
+++ lib/time_solat.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable, unused_import
+
 import 'package:flutter/material.dart';
 import 'package:adhan_dart/adhan_dart.dart';
 import 'package:flutter_quran/var.dart';
